2274416317cd3f011d5e7932a2ebcd338a53f031
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2022-06-14 17:12:43 +0400

    Added multiprocessing

--- run_LPIPS.py
+++ run_LPIPS.py
@@ -7,75 +7,87 @@ from torchvision import models
 from tqdm.auto import tqdm
 import pandas as pd
 import torch
+from torch.multiprocessing import set_start_method
+import torch.multiprocessing as mp
 
 
 def normalize(x):
-    return (2 * (x - x.min()) / (x.max() - x.min()) - 1).cuda()
+    return 2 * (x - x.min()) / (x.max() - x.min()) - 1
 
-if __name__ == '__main__':
-    res_path = './results'
-    scores_path = './scores_non_red'
+def evaluate(rank, files, res_path, scores_path, k_list, p_count):
+    results = {'eps': [], 'k':[], 'device': [], 'score': [], 'score_pert': [], 'miss_pert': [], 'score_gauss': [], 'miss_gauss': [], 'miss': []}
 
-    files = os.listdir(res_path)
-    files = [f for f in files if not f.endswith('dtl.pt') and not f.endswith('prt.pt')]
-
-    lpips = NoTrainLpips(net='vgg', verbose=False).cuda()
-    lpips_score = lambda x, y: lpips(normalize(x), normalize(y))
-
-    k = 1 #16
-    p_count = 1 #10
-    model = models.vgg16(pretrained=True).cuda()
-    model.eval()
-
-    results = {'eps': [], 'device': [], 'score': [], 'score_pert': [], 'miss_pert': [], 'score_gauss': [], 'miss_gauss': [], 'miss': []}
-
+    files = [f for f in files if int(f.split('_')[-1].replace(".pt", "")) == rank]
     for f in tqdm(files):
         fparam = f.split('_')
         device = int(fparam[-1].replace(".pt", ""))
         dataloader = torch.load(os.path.join(res_path, f'{fparam[0]}_{device}_dtl.pt'))
         perturb = torch.load(os.path.join(res_path, f.replace('.pt', '_prt.pt')))
-        perturb = perturb.cuda()
+        perturb = perturb.cuda(rank)
         adv = torch.load(os.path.join(res_path, f))
         bs = dataloader.batch_size
 
+        lpips = NoTrainLpips(net='squeeze', verbose=False).cuda(rank)
+        lpips_score = lambda x, y: lpips(normalize(x.cuda(rank)), normalize(y.cuda(rank)))
+        model = models.alexnet(pretrained=True).cuda(rank)
+        model.eval()
+
         for i, (x, y) in tqdm(enumerate(dataloader), leave=False):
-            x = x.cuda()
-            adv_sample = adv[i*bs:(i+1)*bs].cuda()
+            x = x.cuda(rank)
+            adv_sample = adv[i * bs:(i + 1) * bs].cuda(rank)
 
             scores = list(lpips_score(x, adv_sample).detach().cpu().numpy().reshape(-1))
             predictions = model.forward(x)
             pred_adv = model.forward(adv_sample)
-            miss = list((torch.argmax(predictions, dim=1)  != torch.argmax(pred_adv, dim=1)).detach().cpu().numpy())
-            results['score'].extend(scores * p_count)
-            results['device'].extend([device] * len(scores) * p_count)
-            results['eps'].extend([float(fparam[2])] * len(scores) * p_count)
-            results['miss'].extend(miss * p_count)
+            miss = list((torch.argmax(predictions, dim=1) != torch.argmax(pred_adv, dim=1)).detach().cpu().numpy())
+            results['score'].extend(scores * p_count * len(k_list))
+            results['device'].extend([device] * len(scores) * p_count * len(k_list))
+            results['eps'].extend([float(fparam[2])] * len(scores) * p_count * len(k_list))
+            results['miss'].extend(miss * p_count * len(k_list))
 
-            for _ in tqdm(range(p_count), leave=False):
-                perm = torch.randperm(perturb.size(0))
-                idx = perm[:k]
-                samples = perturb[idx]
-                pert_images = x + samples.mean(dim=0)
+            for k in tqdm(k_list, leave=False):
+                results['k'].extend([k] * len(scores) * p_count)
+                for _ in tqdm(range(p_count), leave=False):
+                    perm = torch.randperm(perturb.size(0))
+                    idx = perm[:k]
+                    samples = perturb[idx]
+                    pert_images = x + samples.mean(dim=0)
 
-                pred_adv = model.forward(pert_images)
-                miss_pert = list((torch.argmax(predictions, dim=1) != torch.argmax(pred_adv, dim=1)).detach().cpu().numpy())
-                results['score_pert'].extend(list(lpips_score(x, pert_images).detach().cpu().numpy().reshape(-1)))
-                results['miss_pert'].extend(miss_pert)
+                    pred_adv = model.forward(pert_images)
+                    miss_pert = list((torch.argmax(predictions, dim=1) != torch.argmax(pred_adv, dim=1)).detach().cpu().numpy())
+                    results['score_pert'].extend(list(lpips_score(x, pert_images).detach().cpu().numpy().reshape(-1)))
+                    results['miss_pert'].extend(miss_pert)
 
-            for _ in tqdm(range(p_count), leave=False):
-                noise = torch.normal(mean=0., std=float(fparam[2]), size=x.shape[1:])
-                noise = torch.clip(noise, -float(fparam[2]), float(fparam[2])).cuda()
-                pert_images = x + noise
+                for _ in tqdm(range(p_count), leave=False):
+                    noise = torch.normal(mean=0., std=float(fparam[2]), size=x.shape[1:])
+                    noise = torch.clip(noise, -float(fparam[2]), float(fparam[2])).cuda(rank)
+                    pert_images = x + noise
 
-                pred_adv = model.forward(pert_images)
-                miss_gauss = list((torch.argmax(predictions, dim=1) != torch.argmax(pred_adv, dim=1)).detach().cpu().numpy())
-                results['score_gauss'].extend(list(lpips_score(x, pert_images).detach().cpu().numpy().reshape(-1)))
-                results['miss_gauss'].extend(miss_gauss)
-
+                    pred_adv = model.forward(pert_images)
+                    miss_gauss = list(
+                        (torch.argmax(predictions, dim=1) != torch.argmax(pred_adv, dim=1)).detach().cpu().numpy())
+                    results['score_gauss'].extend(list(lpips_score(x, pert_images).detach().cpu().numpy().reshape(-1)))
+                    results['miss_gauss'].extend(miss_gauss)
 
         print(f'Eps: {fparam[2]}')
         print(f'scores_mean {np.mean(results["score"])}')
         print(f'pt_scores_mean {np.mean(results["score_pert"])}')
         print(f'rn_scores_mean {np.mean(results["score_gauss"])}')
 
-        pd.DataFrame.from_dict(results).to_csv(os.path.join(scores_path, f'results.csv'))
+        pd.DataFrame.from_dict(results).to_csv(os.path.join(scores_path, f'results_{rank}.csv'))
+
+
+if __name__ == '__main__':
+    res_path = './results'
+    scores_path = './scores_alex_alex'
+
+    file_list = os.listdir(res_path)
+    file_list = [f for f in file_list if not f.endswith('dtl.pt') and not f.endswith('prt.pt')]
+
+
+
+    k_list = [1] #16
+    p_count = 10 #10
+    nprocs = 8
+
+    mp.spawn(fn=evaluate, args=(file_list, res_path, scores_path, k_list, p_count), nprocs=nprocs, )
