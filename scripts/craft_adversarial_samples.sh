1578af2ad7179a814689aa03bf3db0d5c0c59eac
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2022-07-17 15:30:21 +0400

    Edited scripts to run Imporved Wasserstein as well

--- scripts/craft_adversarial_samples.sh
+++ scripts/craft_adversarial_samples.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 echo "Generating adversarial samples using PGD"
+mkdir "results"
 python generate.py
 
 echo "Generating adversarial samples using Wasserstein Frank-Wolfe + Dual LMO"
+mkdir "results_wass"
 (cd ../fast_wasserstein_adversarial && python generate.py)
-mv ../../fast_wasserstein_adversarial/results_wass/ .
-cp results/pgd_*_dtl.pt results_wass/
+mv ../fast_wasserstein_adversarial/results_wass/ .
+
+echo "Generating adversarial samples using Improved Wasserstein"
+mkdir "results_iwass"
+(cd ../improved_wasserstein && python generate.py)
+mv ../improved_wasserstein/results_iwass/ .
--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -3,3 +3,4 @@
 scripts/craft_adversarial_samples.sh
 scripts/get_LPIPS_scores.sh results pgd_scores
 scripts/get_LPIPS_scores.sh results_wass wass_scores
+scripts/get_LPIPS_scores.sh results_iwass iwass_scores
