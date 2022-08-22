f082ea8ba239db8df07ce4d861994bdccafaeace
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2022-08-22 20:02:23 +0400

    Added script to download repos and set up env

---
+++ scripts/setup_arena.sh
@@ -0,0 +1,14 @@
+(cd .. && git clone https://github.com/bezirganyan/fast-wasserstein-adversarial.git fast-wasserstein-adversarial)
+(cd ../fast-wasserstein-adversarial && git switch LPIPS_experiments)
+(cd .. && git clone https://github.com/bezirganyan/improved_wasserstein.git improved_wasserstein)
+
+mkdir data
+wget https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-320.tgz data/imagenette2-320.tgz
+(cd data && tar -xzf data/imagenette2-320.tgz)
+rm data/imagenette2-320.tgz
+
+conda create -n "adversarial-arena" python=3.9.7
+conda activate "adversarial-arena"
+pip install -r requirements.txt
+
+(cd ../fast-wasserstein-adversarial/sparse_tensor && python setup.py install)
